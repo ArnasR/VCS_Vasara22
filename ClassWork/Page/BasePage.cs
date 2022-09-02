b8db31dd99956e397b1fad158050143a5f0a9e0d
ArnasR <ratkevicius.arnas@gmail.com>
2022-09-02 19:08:34 +0300

    Added BasePage

---
+++ ClassWork/Page/BasePage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork.Page
+{
+    class BasePage
+    {
+        protected static IWebDriver Driver;
+
+        public BasePage(IWebDriver webDriver)
+        {
+            Driver = webDriver;
+        }
+
+        public WebDriverWait GetWait(int seconds = 5)
+        {
+            return new WebDriverWait(Driver, TimeSpan.FromSeconds(seconds));
+        }
+
+        public void ScrollDown()
+        {
+            ((IJavaScriptExecutor)Driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 150)");
+        }
+    }
+}
