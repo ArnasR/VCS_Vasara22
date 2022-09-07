3a0aee7638510cb2dc95942772ab20e6fa45705f
ArnasR <ratkevicius.arnas@gmail.com>
2022-09-07 19:17:43 +0300

    Added SenukaiPage class

---
+++ ClassWork/Page/SenukaiPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork.Page
+{
+    class SenukaiPage : BasePage
+    {
+        private const string PageAddress = "https://www.senukai.lt/";
+
+        public SenukaiPage(IWebDriver webDriver) : base(webDriver) { }
+
+        public void NavigateToDefaultPage()
+        {
+            if (Driver.Url != PageAddress)
+            {
+                Driver.Url = PageAddress;
+            }
+        }
+
+        public SenukaiPage AcceptCookies()
+        {
+            Cookie myCookie = new Cookie("CookieConsent",
+                "{stamp:%27PXyoyHsd8h2rWSbfWRSYw1Hw0FWHWxeMFkmENfnuWJ+qQHyMpDTDaw==%27%2Cnecessary:true%2Cpreferences:false%2Cstatistics:false%2Cmarketing:true%2Cver:1%2Cutc:1662566380304%2Cregion:%27lt%27}",
+                "www.senukai.lt",
+                "/",
+                DateTime.Now.AddDays(1));
+
+            Driver.Manage().Cookies.AddCookie(myCookie);
+            Driver.Navigate().Refresh();
+
+            return this;
+        }
+    }
+}
